7e3bfbd8a1630fcdd078d981a91c5ed1d0e9025d
Matt Cadorette <mcadorette@gmail.com>
2018-11-06 13:35:06 -0800

    Initial commit from Atomist

---
+++ src/main/java/qcon444/Qcon444Application.java
@@ -0,0 +1,12 @@
+package qcon444;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qcon444Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qcon444Application.class, args);
+	}
+}
